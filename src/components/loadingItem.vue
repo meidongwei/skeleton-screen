27e9b7235dba9b2b32eeb3c3e2e8c836f4c9f54c
Meidongwei <450650472@qq.com>
2018-05-11 17:33:12 +0800

    update some

--- src/components/loadingItem.vue
+++ src/components/loadingItem.vue
@@ -22,7 +22,7 @@ export default {
   background: #fff;
   /* border: 1px solid; */
   /* border-color: #e5e6e9 #dfe0e4 #d0d1d5; */
-  border-radius: 3px;
+  /* border-radius: 3px; */
   padding: 12px;
   margin: 0 auto;
   max-width: 472px;
